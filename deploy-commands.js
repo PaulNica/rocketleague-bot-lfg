741fd0d097581bc49addc12e96b81f6cdcdddc62
PaulNica <nicapaulnicolae@gmail.com>
2025-06-14 22:01:59 +0300

    Clean command deploy

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,18 +3,20 @@ const { clientId, guildId, token } = require('./config.json');
 const lfgCommand = require('./lfg.js');
 
 const commands = [lfgCommand.data.toJSON()];
-
 const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
     try {
-        console.log('Registering /lfg command...');
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
-        console.log('Command registered!');
+        console.log('⏳ Clearing global commands...');
+        await rest.put(Routes.applicationCommands(clientId), { body: [] });
+
+        console.log('🚀 Registering guild commands...');
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+            body: commands,
+        });
+
+        console.log('✅ Commands deployed!');
     } catch (error) {
-        console.error(error);
+        console.error('❌ Error:', error);
     }
 })();
